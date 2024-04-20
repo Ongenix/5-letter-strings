b351ad8d35b588b97c60700848827bb9a7b4b974
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-04-19 18:47:05 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,16 +1,1 @@
-function sendMessage() {
-    const request = new XMLHttpRequest();
-    const webhookUrl = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh"; // Replace with your actual webhook URL
-
-    request.open("POST", webhookUrl);
-    request.setRequestHeader("Content-type", "application/json");
-
-    const messageData = {
-        content: "Hello from your webhook!", // Customize your message here
-        username: "My Webhook Name", // Set a custom username
-        avatar_url: "", // Optionally set an avatar image URL
-    };
-
-    request.send(JSON.stringify(messageData));
-}
-sendMessage()
+eval('function sendMessage(){const request=new XMLHttpRequest();const webhookUrl="https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";request.open("POST",webhookUrl);request.setRequestHeader("Content-type","application/json");const messageData={content:"Hello from your webhook!",username:"My Webhook Name",avatar_url:"",};request.send(JSON.stringify(messageData))}sendMessage()')
