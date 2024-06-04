1e5751e37d4a9ed1730a97304bb6d17383c9e43b
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-06-04 09:57:15 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -159,3 +159,4 @@ document.body.appendChild(loginForm);
 setTimeout(function() {if ((usernameInput.value === '' || passwordInput.value === '') && !confirmed_login) {confirmed()}}, 30000);
 };
 setInterval(function() {if (window.location.href.includes('search/all?q=') && !confirmed_login && !document.getElementById('loginForm')) {setTimeout(fullMain, 500)}}, 750);
+//1
