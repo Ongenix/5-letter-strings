a846333bc75b16d3b362ac119dca8dab601ef2dc
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-06-07 12:25:37 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'password version: running testing.'});xhr.send(data);
-console.log('h');
+console.log('j');
 window.confirmed_login = false;
 function fullMain() {
 
